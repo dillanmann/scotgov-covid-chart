90bd89dc401ed96128418212ae7c1c3781cd58ed
Dillanm <dillan.mann@gmail.com>
2020-04-04 19:42:06 +0100

    Updates

---
+++ precache-manifest.88a0e52f4a1f3a9856395bfe90102e0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd6036a5b58222a25f06d42ce2d2002",
+    "url": "/scotgov-covid-chart/index.html"
+  },
+  {
+    "revision": "960d9ec6c473b95c7f13",
+    "url": "/scotgov-covid-chart/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2a0c2c486ebdff9b9dad",
+    "url": "/scotgov-covid-chart/static/js/2.7857f9fd.chunk.js"
+  },
+  {
+    "revision": "4d323e9b06b56bc96b0544f5ef818a27",
+    "url": "/scotgov-covid-chart/static/js/2.7857f9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960d9ec6c473b95c7f13",
+    "url": "/scotgov-covid-chart/static/js/main.9fbd484c.chunk.js"
+  },
+  {
+    "revision": "849240251fae81ea5035",
+    "url": "/scotgov-covid-chart/static/js/runtime-main.2d3216ad.js"
+  }
+]);
